d4ac0a7542c5089bd1fe3315c46e97c8c4f6dae7
Dmytro Sheleh <dmytro@asimovprotocol.org>
2025-02-04 18:07:04 +0100

    chore: serve fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,50 @@ import Nav from "@/components/Nav";
 import { WalletSelectorProvider } from "@/context/WalletSelectorContext";
 import { BreadcrumbProvider } from "@/context/BreadcrumbContext";
 import Footer from "@/components/Footer";
+import localFont from 'next/font/local';
 
 export const metadata: Metadata = {
   title: "Asimov Directory",
   description: "",
 };
+
+const suisseIntl = localFont({
+  src: [
+    {
+      path: '../assets/fonts/SuisseIntl-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SuisseIntl-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/SuisseIntl-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-suisse-intl', // optional
+});
+
+// Arges configuration
+const arges = localFont({
+  src: [
+    {
+      path: '../assets/fonts/Arges-NormalMedium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/Arges-NormalBlack.woff2',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-arges',
+});
 
 type Props = {
   children: Readonly<React.ReactNode>;
@@ -18,7 +57,7 @@ type Props = {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className={`${suisseIntl.variable} ${arges.variable} antialiased`}>
         <WalletSelectorProvider>
           <Nav />
           <main className="relative ">
