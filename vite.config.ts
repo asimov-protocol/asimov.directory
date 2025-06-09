5efeddc2f84213f9ea8cb88059808f73be88063a
Dmytro Sheleh <sheleg.dmitriy@gmail.com>
2025-06-09 22:57:26 +0200

    chore: add deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import path from 'path';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: path.resolve(__dirname, 'CNAME'),
+					dest: '.'
+				}
+			]
+		})
+	]
 });
