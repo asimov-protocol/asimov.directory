f5cf10798787258ee2e71e5f5095753a432e43e7
Dmytro Sheleh <sheleg.dmitriy@gmail.com>
2025-06-23 14:43:43 +0200

    chore: create config file

---
+++ src/lib/config.ts
@@ -0,0 +1,15 @@
+export const GITHUB_API_BASE = 'https://api.github.com';
+export const GITHUB_BASE_URL = 'https://github.com';
+export const ASIMOV_MODULES_ORG_NAME = 'asimov-modules';
+export const ASIMOV_PROTOCOL_ORG_NAME = 'asimov-protocol';
+export const ASIMOV_DIRECTORY_REPO_NAME = 'asimov.directory';
+
+export const EXCLUDED_REPOS = [
+	'.github',
+	'asimov-modules.rb',
+	'asimov-modules.py',
+	'asimov-goodreads-module'
+];
+
+export const directoryGithubUrl = `${GITHUB_BASE_URL}/${ASIMOV_PROTOCOL_ORG_NAME}/${ASIMOV_DIRECTORY_REPO_NAME}`;
+export const asimovProtocolOrgUrl = `${GITHUB_BASE_URL}/${ASIMOV_PROTOCOL_ORG_NAME}`;
