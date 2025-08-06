2ba518d650dd7ed1e1177e6d498f1fc2d56cb06a
Dmytro Sheleh <sheleg.dmitriy@gmail.com>
2025-08-06 10:45:47 +0200

    fix: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,9 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
